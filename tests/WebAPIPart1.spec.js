a5d95552c5bd723f1ab633729a067308bd2b61e9
nikhil <nikhilsharma270027@gmail.com>
2026-06-24 19:55:48 +0530

    complete section 11

--- tests/WebAPIPart1.spec.js
+++ tests/WebAPIPart1.spec.js
@@ -1,56 +1,34 @@
 import { test, expect, request } from "@playwright/test";
-import { APIUtils } from "../utils/APIUtils";
+import APIUtils from "./utils/APIUtils";
 
 const loginPayload = { userEmail: "anshika@gmail.com", userPassword: "Iamking@000" };
 const orderPayload = { orders: [{ country: "India", productOrderedId: "6960eae1c941646b7a8b3ed3" }] };
 let token;
 let orderId;
+
+let response ;
 // beforeAll and beforeEach hooks are used to set up the test environment before running the tests.
 // The beforeAll hook runs once before all tests in the file,
 test.beforeAll(async () => {
   // Login API call to get the auth token
   const apiContext = await request.newContext();
-  // const response = await apiContext.post("https://www.rahulshettyacademy.com/api/ecom/auth/login", {
-  //   data: loginPayload,
-  // });
-  // // console.log("Response", response);
+  const apiUtils = new APIUtils(apiContext, loginPayload);
+  console.log("API Utils:", apiUtils);
+  response = await apiUtils.createOrder(orderPayload);
+  console.log("Response BeforeAll:", response);
+}, );
 
-  // // expect(response.status()).toBe(200);
-  // expect(response.ok()).toBeTruthy();
-
-  // const responseJson = await response.json();
-  // // console.log(responseJson);
-  // const authToken = responseJson.token;
-  // token = authToken;
-  // console.log(token);
-
-  // APIUtils class to handle API calls
-  // const apiUtils =
-
-  // Order Api call to get the order details
-  const orderResponse = await apiContext.post("https://www.rahulshettyacademy.com/api/ecom/order/create-order", {
-    data: orderPayload,
-    headers: {
-      Authorization: token,
-      "Content-Type": "application/json",
-    },
-  });
-
-  const orderResponseJson = await orderResponse.json();
-  orderId = orderResponseJson.orders[0];
-  console.log("Order ID:", orderId);
-});
 
 // beforeEach hook runs before each individual test.
 test.beforeEach(() => {});
 
 test.only("Client App login", async ({ page }) => {
   // ApiUtils class to handle API calls
-  const apiUtils = new ApiUtils(apiContext, loginPayload);
-  const orderId = await apiUtils;
+  // const apiUtils = new ApiUtils(apiContext, loginPayload);
+  // const orderId = await apiUtils.createOrder(token);
   await page.addInitScript((token) => {
     window.localStorage.setItem("token", token);
-  }, token);
+  }, response.token);
 
   await page.goto("https://rahulshettyacademy.com/client/");
   const productName = "ZARA COAT 3";
@@ -68,15 +46,15 @@ test.only("Client App login", async ({ page }) => {
   // interview question important
   for (let i = 0; i < rowCount; i++) {
     const rowOrderId = await rows.nth(i).locator("th").textContent();
-    console.log(rowOrderId);
-    if (orderId.includes(rowOrderId)) {
+    // console.log(rowOrderId);
+    if (response.orderId.includes(rowOrderId)) {
       await rows.nth(i).locator("button").first().click();
       break;
     }
   }
 
   const orderIdDetails = await page.locator(".col-text").textContent();
-  console.log(orderIdDetails);
-  expect(orderId.includes(orderIdDetails)).toBeTruthy();
+  // console.log(orderIdDetails);
+  expect(response.orderId.includes(orderIdDetails)).toBeTruthy();
   await page.pause();
 });
--- tests/utils/APIUtils.js
+++ tests/utils/APIUtils.js
@@ -1,37 +1,45 @@
-export class APIUtils {
-  constructor(apiContext) {
+import { test, expect, request } from "@playwright/test";
+class APIUtils {
+  constructor(apiContext, loginPayload) {
     this.apiContext = apiContext;
+    this.loginPayload = loginPayload;
   }
   async getToken() {
     const response = await this.apiContext.post("https://www.rahulshettyacademy.com/api/ecom/auth/login", {
-      data: loginPayload,
+      data: this.loginPayload,
     });
     console.log("Response", response);
 
     // expect(response.status()).toBe(200);
-    expect(response.ok()).toBeTruthy();
+    await expect(response.ok()).toBeTruthy();
 
     const responseJson = await response.json();
     // console.log(responseJson);
     const authToken = responseJson.token;
-    token = authToken;
+    let token = authToken;
     console.log(token);
     return token;
   }
 
-  async createOrder(token) {
+  async createOrder(orderPayload) {
     // Order Api call to get the order details
+    let response = {};
+    response.token = await this.getToken();
     const orderResponse = await this.apiContext.post("https://www.rahulshettyacademy.com/api/ecom/order/create-order", {
       data: orderPayload,
       headers: {
-        Authorization: this.getToken(),
+        Authorization: response.token,
         "Content-Type": "application/json",
       },
     });
 
     const orderResponseJson = await orderResponse.json();
-    orderId = orderResponseJson.orders[0];
+    const orderId = orderResponseJson.orders[0];
     console.log("Order ID:", orderId);
-    return orderId;
+    response.orderId = orderId;
+
+    return response;
   }
 }
+
+export default APIUtils;
